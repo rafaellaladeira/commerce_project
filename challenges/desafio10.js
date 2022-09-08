c1c7f852af7f184ace1c0d75849a8777ff5baf6f
rafaellaladeira <rafaella.ladeira@gmail.com>
2022-09-08 18:30:31 -0300

    fix: correção do requisito 10 + requisito 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,5 +2,5 @@ db.produtos.find({
     $and: [
         { "valoresNutricionais.3.percentual": { $gte: 30 } },
         { "valoresNutricionais.3.percentual": { $lte: 40 } },
-    ]
+    ],
 }, { nome: 1, _id: 0 });
