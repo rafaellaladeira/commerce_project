d9d329b501f718a243eab3b0a4fafc2cb56837d1
rafaellaladeira <rafaella.ladeira@gmail.com>
2022-09-11 09:35:14 -0300

    fix: correção de lint req 15

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,20 +1,20 @@
 db.produtos.updateMany({}, {
     $set: {
-      "avaliacao": 0,
+      avaliacao: 0,
     },
 });
 db.produtos.updateMany({
-tags: "bovino",
-}, {
-$inc: {
-  avaliacao: 5,
-}
+    tags: "bovino",
+    }, {
+        $inc: {
+        avaliacao: 5,
+    },
 });
 db.produtos.updateMany({
-tags: "ave",
+    tags: "ave",
 }, {
-$inc: {
-  avaliacao: 3,
-}
+    $inc: {
+    avaliacao: 3,
+    },
 });
 db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
